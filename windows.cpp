3bdebb608f91ed4337dbee4f8cb6ef938290ea8d
modmuss50 <modmuss50@gmail.com>
2025-06-18 21:09:47 +0100

    Add Microsoft WIL dependency

--- windows.cpp
+++ windows.cpp
@@ -2,19 +2,18 @@
 
 #include <windows.h>
 
+// Include after Windows.h
+#include <wil/resource.h>
+
 constexpr LPCWSTR MinecraftLauncherMutexName = L"MojangLauncher";
 
-JNIEXPORT jboolean JNICALL Java_net_fabricmc_installer_launcher_MojangLauncherHelper_isMojangLauncherOpen(JNIEnv *, jclass) {
-    HANDLE handle = OpenMutexW(0, false, MinecraftLauncherMutexName);
+JNIEXPORT jboolean JNICALL
+Java_net_fabricmc_installer_launcher_MojangLauncherHelper_isMojangLauncherOpen(
+    JNIEnv*, jclass) {
+    wil::unique_handle handle{ ::OpenMutexW(0, false, MinecraftLauncherMutexName) };
 
-    if (GetLastError() == ERROR_FILE_NOT_FOUND)
-    {
+    if (GetLastError() == ERROR_FILE_NOT_FOUND) {
         return 0;
-    }
-
-    if (handle != nullptr)
-    {
-        CloseHandle(handle);
     }
 
     return 1;
